18234a145ba2eab00ac6211b3461e91d25717f35
KapilQadir <abdulk.qadir@gmail.com>
2025-04-23 22:57:24 +0530

    aws test1

--- server/app.js
+++ server/app.js
@@ -1,11 +1,50 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const http  = require('http')
+const Server  = require("socket.io").Server
+const app = express()
+const path  = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express server!');
-});
+const server  = http.createServer(app)
+const io = new Server(server , {
+    cors:{
+        origin:"*"
+    }
+})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../react-aws-ec2-nginx/build");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../react-aws-ec2-nginx/build/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
+
+
+
+
+io.on("connection" , (socket) => {
+   console.log('We are connected')
+
+   socket.on("chat" , chat => {
+      io.emit('chat' , chat)
+   } )
+
+   socket.on('disconnect' , ()=> {
+    console.log('disconnected')
+   })
+})
+
+
+
+server.listen(5001 , () => console.log('Listening to port 5001'))
